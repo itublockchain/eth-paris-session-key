2bdda2d312ae10963e9d3ee991250894a1f69dbb
Ali Eray <alieraay@gmail.com>
2023-07-22 13:32:29 +0200

    Game contracts

---
+++ game-contracts/contracts/GameContractFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+import "./GameContract.sol";
+
+contract GameFactory {
+    CardGame[] public games;
+
+    event GameCreated(address indexed gameAddress, uint indexed gameId);
+    
+    function createNewGame() public {
+        CardGame cardGame = new CardGame();
+        games.push(cardGame);
+
+        emit GameCreated(address(cardGame), games.length - 1);
+    }
+
+    function getNumberOfGames() public view returns(uint) {
+        return games.length;
+    }
+
+    function getGameAddress(uint index) public view returns(address) {
+        require(index < games.length, "Invalid index");
+        return address(games[index]);
+    }
+}
